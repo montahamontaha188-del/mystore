896b4e7e73c82328eda563bb30088762649a8d84
montaha <montahamontaha188@gmail.com>
2026-06-01 06:27:27 -0700

    organized code into separate files

---
+++ product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace MyStoreSystem { 
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double Price { get; set; }
+    public int Quantity { get; set; }
+}
+
+}
